6777319fa524235dbd16d28b2b26cf1a45c5573a
ilhan-moha <mohamudilhan99@gmail.com>
2026-03-23 10:39:40 +0300

    added function

--- index.js
+++ index.js
@@ -5,3 +5,39 @@
 
 // This is required for the test to function properly  
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+
+function calculateTax (amount) {
+    const tax = amount * 0.1; 
+     return tax;
+}
+
+function convertToUpperCase (text) {
+    return text.toUpperCase();
+}
+
+function findMaximum (num1, num2) {
+    if (num1 > num2) {
+        return num1;
+    } else {
+        return num2;
+    }
+}
+
+function isPalindrome (word) {
+    if (word === word.split('').reverse().join('')) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function calculateDiscountedPrice (originalPrice, discountPercentage) {
+    const discountAmount = originalPrice * (discountPercentage / 100);
+    return originalPrice - discountAmount;
+}
+
+console.log(calculateTax(100));
+console.log(convertToUpperCase("hello")); 
+console.log(findMaximum(5, 10)); 
+console.log(isPalindrome("racecar")); 
+console.log(calculateDiscountedPrice(100, 20)); 
